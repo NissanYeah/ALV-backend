21ff6549c215c126582b0dab4fa0e3b00ad22694
Nissen <handeltonido@gmail.com>
2020-06-14 15:16:54 +0800

    新增CORS

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
 
+const corsOptions = process.env.NODE_ENV == "development" ? 
+{
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+} : {
+  "origin": ['http://avl-exam.surge.sh/'],
+  "optionsSuccessStatus": 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
 app.use(bodyParser.json());
-app.use('/mail', require('./routes/mail'))
+app.use('/mail', cors(corsOptions),require('./routes/mail'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
