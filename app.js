1acfe3a121eb4ad89da75f22e5c5e7948a5a4d12
Nissen <handeltonido@gmail.com>
2020-06-14 15:23:51 +0800

    調整調整CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const corsOptions = process.env.NODE_ENV == "development" ? 
 {
